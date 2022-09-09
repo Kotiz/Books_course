84255e5791f345eb40775528106ab26125235a84
Kotiz <analogiasnu@gmail.com>
2022-09-09 18:34:31 +0200

    not working button Add book

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <header>
+      <h1>Books<span>.app</span></h1>
+    </header>
+    <ul>
+      <li v-for="(book, index) in books" :key="index">
+        {{ book.title }}, {{ book.price }}$
+        <button @click="removeBook">Delete book</button>
+      </li>
+    </ul>
+    <p v-show="!books.length">No books...</p>
+    <form @submit.prevent="handleSubmit">
+      <label>
+        Title:
+        <input v-model="form.title" type="text" name="title">{{ form.title }}
+      </label>
+      <label>
+        Price:
+        <input v-model="form.price" type="number" name="price">{{ form.price }}
+      </label>
+      <button>Add book </button>
+    </form>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: () => ({
+    books: [
+      {
+        title: 'Windows Powershell w miesiąc',
+        price: 20
+      },
+      {
+        title: 'Alicja w krainie czarów',
+        price: 18
+      }
+    ],
+    form: {
+      title: '',
+      price: null
+    }
+  }),
+  methods: {
+    removeBook (index) {
+      this.books.splice(index, 1)
+      console.log('delete')
+    }
+  },
+  handleSubmit () {
+    this.books.push({ ...this.form })
+    console.log('submit')
   }
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
+  li {
+    padding: 5px;
+  };
 </style>
