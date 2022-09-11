fc24658d0546ba1487d02587b90be49531ab2eb9
Kotiz <analogiasnu@gmail.com>
2022-09-11 13:36:17 +0200

    Dodałem komponent BookForm / Amount i Total Price działa

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
 <template>
-  <div id="app">
+  <div class="app">
     <header>
-      <h1>Books<span>.app</span></h1>
+      <h1 class="app__heading">Books<span>.app</span></h1>
     </header>
     <books-list @remove="removeBook" :books="books" />
     <books-length-msg :books="books"/>
-    <form @submit.prevent="handleSubmit">
-      <label>
-        Title:
-        <input v-model="form.title" type="text" name="title">
-      </label>
-      <label>
-        Price:
-        <input v-model="form.price" type="number" name="price">
-      </label>
-      <button type="submit">Add book</button>
-    </form>
+    <book-form @add="addBook"/>
   </div>
   <books-summary :books="books" />
 </template>
@@ -23,9 +13,10 @@
 import BooksList from './components/BooksList.vue'
 import BooksLengthMsg from './components/BooksLengthMsg.vue'
 import booksSummary from './components/BookSummary.vue'
+import BookForm from './components/BookForm.vue'
 // import booksItem from './components/BooksItems.vue'
 export default {
-  components: { BooksList, BooksLengthMsg, booksSummary },
+  components: { BooksList, BooksLengthMsg, booksSummary, BookForm },
   name: 'App',
   data: () => ({
     books: [
@@ -41,31 +32,31 @@ export default {
         title: 'Dracula',
         price: 10
       }
-    ],
-    form: {
-      title: '',
-      price: 0
-    }
+    ]
   }),
   methods: {
+    addBook (book) {
+      this.books.push({ ...book })
+    },
     removeBook (index) {
       this.books.splice(index, 1)
       console.log('delete', index)
-    },
-    handleSubmit () {
-      const newBook = { ...this.form }
-      this.books.push(newBook)
-      this.resetForm()
-    },
-    resetForm () {
-      this.form.price = null
-      this.form.title = ''
     }
   }
 }
 </script>
-<style>
-  li {
-    padding: 5px;
-  };
-</style>
+<style lang="scss">
+  .app {
+    width: 100%;
+    max-width: 1000px;
+    padding: 2rem;
+    margin: 0 auto;
+      &__heading {
+      font-size: 3rem;
+      text-align: center;
+      span {
+        color: #5a58da;
+      }
+    }
+  }
+  </style>
